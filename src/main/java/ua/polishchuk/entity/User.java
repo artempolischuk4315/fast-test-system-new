eaa111ffb7571bbbf634bd49e994022fb3454e73
Artem Polishchuk <art4315@gmail.com>
2020-08-18 11:29:20 +0300

    Add User entity

---
+++ src/main/java/ua/polishchuk/entity/User.java
@@ -0,0 +1,41 @@
+package ua.polishchuk.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+
+@Entity
+@Table( name="user",
+        uniqueConstraints={@UniqueConstraint(columnNames={"email"})})
+public class User  {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column( nullable = false)
+    private String firstName;
+
+    @Column( nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role roles;
+
+    private double statistics;
+
+}
